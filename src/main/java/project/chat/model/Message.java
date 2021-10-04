4739e7e12770d9af413f743108a3f9e0631c5da1
Jkinjt <l02jitoj@gmail.com>
2021-10-04 14:03:11 +0200

    as

--- src/main/java/project/chat/model/Message.java
+++ src/main/java/project/chat/model/Message.java
@@ -7,5 +7,49 @@ public class Message {
 	private Date date;
 	private String message;
 	private Room room;
+	
+	
+	public Message() {
+		super();
+	}
+
+
+	public Message(User user, Date date, String message, Room room) {
+		super();
+		this.user = user;
+		this.date = date;
+		this.message = message;
+		this.room = room;
+	}
+
+
+	public User getUser() {
+		return user;
+	}
+
+
+	public Date getDate() {
+		return date;
+	}
+
+
+	public String getMessage() {
+		return message;
+	}
+
+
+	public Room getRoom() {
+		return room;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Message [user=" + user + ", date=" + date + ", message=" + message + ", room=" + room + "]";
+	}
+	
+	
+	
+	
 
 }
--- src/main/java/project/chat/model/UserRoom.java
+++ src/main/java/project/chat/model/UserRoom.java
@@ -1,9 +1,22 @@
 package project.chat.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserRoom {
 	private List<User> users;
 	private List<Room> rooms;
+	private static UserRoom singletoon;
+	
+	private UserRoom() {
+		users=new ArrayList<>();
+		rooms=new ArrayList<>();
+	}
+	public static UserRoom get_Instance() {
+		singletoon=new UserRoom();
+		
+		return singletoon;
+		
+	}
 
 }
