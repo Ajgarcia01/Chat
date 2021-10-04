bd59ed3edf469cb38ac116dba8e8a9220cca2183
Ajgarcia01 <77391383+Ajgarcia01@users.noreply.github.com>
2021-10-04 14:33:59 +0200

    Vamos mejor

--- src/main/java/project/chat/App.java
+++ src/main/java/project/chat/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("secondary"), 640, 480);
+        scene = new Scene(loadFXML("pantallaUser"), 1280, 720);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/project/chat/model/UserRoom.java
+++ src/main/java/project/chat/model/UserRoom.java
@@ -4,10 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserRoom {
-<<<<<<< HEAD
 	
-	
-=======
 	private List<User> users;
 	private List<Room> rooms;
 	private static UserRoom singletoon;
@@ -23,5 +20,4 @@ public class UserRoom {
 		
 	}
 
->>>>>>> 4739e7e12770d9af413f743108a3f9e0631c5da1
 }
