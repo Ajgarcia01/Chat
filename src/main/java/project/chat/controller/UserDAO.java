d82ae165483c8b8419e40a70639ceed3ef8243da
Jkinjt <l02jitoj@gmail.com>
2021-10-07 20:39:12 +0200

    UserDAO

---
+++ src/main/java/project/chat/controller/UserDAO.java
@@ -0,0 +1,33 @@
+package project.chat.controller;
+
+import project.chat.model.User;
+
+public class UserDAO extends User{
+	private static UserDAO _Instance;
+
+	private UserDAO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	private UserDAO(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public UserDAO getInstance() {
+		if(_Instance==null) {
+			_Instance=new UserDAO();
+		}
+		return _Instance;
+	}
+	public UserDAO getInstance(String name) {
+		if(_Instance==null) {
+			_Instance=new UserDAO(name);
+		}
+		return _Instance;
+	}
+	
+	
+	
+}
